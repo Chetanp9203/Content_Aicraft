67625dde727470214e653bb07b9027891d48e038
Chetan Patil <chetanp77701.com>
2024-08-02 00:24:57 +0530

    commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,6 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./utils/Schema.tsx",
+    schema:  "./app/utils/Schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
       url: 'postgresql://AICraft_owner:Zm0EK5HrVTOI@ep-odd-hall-a11o6qce.ap-southeast-1.aws.neon.tech/AICraft?sslmode=require',
